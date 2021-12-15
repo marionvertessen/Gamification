c242d9817a955bb5066cb08a8596ffa1d977d6f3
Yohan Michelland <yohan.michelland@etu.univ-lyon1.fr>
2021-12-15 16:32:05 +0100

    debug et ajout score influence hexad (partie 1/2)

--- main.py
+++ main.py
@@ -37,14 +37,62 @@ def score_influence (name_fichier, name_fichier2, eleve):
         score = 0
         #On parcourt les valeurs de MI, ME et AM
         # Si la pvalues est inférieur à 0 on additionne au score l'influence correpsondante
-        if data_pvalues[col][0] < 0.1 :
-            score_MI += data_influences[col][0] * (eleve['micoI'] + eleve['miacI'] + eleve['mistI'])
-        if data_pvalues[col][1] < 0.1 :
-            score_ME += data_influences[col][1] * (eleve['meidI'] + eleve['meinI'] + eleve['mereI'])
-        if data_pvalues[col][2] < 0.1:
-            score_AMOT -= data_influences[col][2] * eleve['amotI']
+        if data_pvalues["MI"][ligne] < 0.1:
+            score_MI += data_influences['MI'][ligne]
+        if data_pvalues["ME"][ligne] < 0.1:
+            score_ME += data_influences['ME'][ligne]
+        if data_pvalues["amotI"][ligne] < 0.1:
+            score_AMOT += data_influences['amotI'][ligne] * eleve['amotI']
+    score_MI *= (eleve['micoI'] + eleve['miacI'] + eleve['mistI'])/3
+    score_ME *= (eleve['meidI'] + eleve['meinI'] + eleve['mereI'])/3
     #print(liste_score)
     return [score_MI, score_ME, score_AMOT]
+
+def score_influence_hexad (name_fichier, name_fichier2, eleve):
+    #Création de la liste des scores pour chaque profil
+    liste_score = []
+
+    #On lit les données des deux fichiers d'influence et de pvalues
+    data_influences = pd.read_csv(name_fichier, sep=";")
+    data_pvalues = pd.read_csv(name_fichier2, sep=";")
+
+    #On enleve la prmeière colonne qui ne sert à rien ici
+    del data_pvalues['Unnamed: 0']
+    del data_influences['Unnamed: 0']
+
+    #On parcourt chaque profil et on regarde les valeurs pertinantes
+    score_achiever = 0
+    score_player = 0
+    score_socialiser = 0
+    score_freespirit = 0
+    score_disruptor = 0
+    score_philantropist = 0
+
+    for ligne in data_pvalues.index:
+        #On intialise le score
+        score = 0
+        #On parcourt les valeurs de MI, ME et AM
+        # Si la pvalues est inférieur à 0 on additionne au score l'influence correpsondante
+        if data_pvalues["achiever"][ligne] < 0.1:
+            score_achiever += data_influences['achiever'][ligne]
+        if data_pvalues["player"][ligne] < 0.1:
+            score_player += data_influences['player'][ligne]
+        if data_pvalues["socialiser"][ligne] < 0.1:
+            score_socialiser += data_influences['socialiser'][ligne]
+        if data_pvalues["freeSpirit"][ligne] < 0.1:
+            score_freespirit += data_influences['freeSpirit'][ligne]
+        if data_pvalues["disruptor"][ligne] < 0.1:
+            score_disruptor += data_influences['disruptor'][ligne]
+        if data_pvalues["philanthropist"][ligne] < 0.1:
+            score_philantropist += data_influences['philanthropist'][ligne]
+    score_achiever *= eleve['achiever']
+    score_player *= eleve['player']
+    score_socialiser *= eleve['socialiser']
+    score_freespirit *= eleve['freeSpirit']
+    score_disruptor *= eleve['disruptor']
+    score_philantropist *= eleve['philanthropist']
+    #print(liste_score)
+    return [score_achiever, score_player, score_socialiser, score_freespirit, score_disruptor, score_philantropist]
 
 def vecteur_influence_Motivation (eleve) :
     #On calcule les différents vecteurs pour chaque type d'éléments ludiques
@@ -63,7 +111,8 @@ def vecteur_influence_Motivation (eleve) :
     #print(vecteur_tot)
     for k in range(len(vecteur_tot)):
         liste_score_joueur.append([k, vecteur_tot[k][0] + vecteur_tot[k][1] - vecteur_tot[k][2]])
-    print(liste_score_joueur)
+        # liste_score_joueur.append([k, vecteur_tot[k][2]])
+    # print(liste_score_joueur)
     liste_score_joueur.sort(key=lambda x: x[1], reverse=True)
 
     #print(liste_score_joueur)
@@ -105,18 +154,9 @@ def vecteur_influence_Hexad (eleve) :
         activite.sort(key=lambda x: x[1], reverse=True)
         #print(activite)
 
-        #On remplit l'influence pour le joueur associé
-        liste_score_joueur.append(activite)
+    liste_score_joueur.sort(key=lambda x: x[1], reverse=True)
+    return liste_score_joueur
 
-    #On remplace les nombre par des nom
-
-    #On remplit les score associé à chaque joueur
-    """influence_achiever.append(liste_score_joueur[0])
-    influence_player.append(liste_score_joueur[1])
-    influence_socialiser.append(liste_score_joueur[2])
-    influence_freeSpirit.append(liste_score_joueur[3])
-    influence_disruptor.append(liste_score_joueur [4])
-    influence_philanthropist.append(liste_score_joueur [5])"""
 
 def replace_name_element (liste):
     for k in range (len(liste)):
